684fb166278cce08c777f87b0fb6068708928c55
ansh bhakuni <86779028+anshbhakuni123go@users.noreply.github.com>
2022-06-29 10:15:05 +0530

    some new updates

---
+++ tm.js
@@ -0,0 +1,9 @@
+setInterval(() => {
+    const date = new Date();
+    document.getElementById(
+      "time"
+    ).innerHTML = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  }, 1000);
+
+
+  
